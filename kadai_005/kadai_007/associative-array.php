e7c2b527321f71061df3b53c5846ea9d7445e613
Haruma0130 <keitamaeda0529@outlook.jp>
2025-01-13 23:32:29 +0900

    Add files via upload

---
+++ kadai_005/kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP課題005</title>
+</head>
+<body>
+  <p>
+    <?php
+    $kadai_007=['name'=>'onion','price'=>200,'weight'=>160];
+
+    print_r($kadai_007);
+    ?>
+  </p>
+</body>
